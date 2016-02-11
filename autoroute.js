3a8944b769135a68be6c915807b8d244acfe5e8d
Didier Franc <mail@didierfranc.com>
2016-02-11 03:24:22 +0100

    Update autoroute.js

--- autoroute.js
+++ autoroute.js
@@ -6,14 +6,14 @@ if ( l.hash == '' ){
   l.hash = '/'
 }
 
-_.route = function route(arg, query, next){
+_.route = function route(route, query, next){
 
   w.addEventListener("hashchange", function(){
 
     var x = l.hash.slice(2)
     var el = document.querySelectorAll(query)[0]
 
-    if ( arg.slice(1) == a[0] ) {
+    if ( route.slice(1) == a[0] ) {
       next(el)
     } else {
       el.innerHTML = ''
