a1a7485ccd2c1b2397697a62625156fd2a79cad1
Didier Franc <mail@didierfranc.com>
2016-02-11 03:13:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ _.route = function route(arg, query, next){
   w.addEventListener("hashchange", function(){
 
     var x = l.hash.slice(2)
-    var a = x.split('/')
     var el = document.querySelectorAll(query)[0]
 
     if ( arg.slice(1) == a[0] ) {
