1e6257ca93573cdeb14d4effc050faea75ae0c52
Didier Franc <mail@didierfranc.com>
2016-02-11 16:35:09 +0100

    Update autoroute.js

--- autoroute.js
+++ autoroute.js
@@ -1,26 +1,32 @@
-var _ = {}
+var route = {}
 var w = window
 var l = w.location
 
+// Register routes on hashchange event
+route.create = function (route, query, next) {
+
+  w.addEventListener("hashchange", function () {
+
+      console.log('fired')
+      var a = l.hash.slice(2).split('/')
+      var el = document.querySelectorAll(query)
+
+      if ( route.slice(1) == a[0] ) {
+        next.call(el[0])
+      } else {
+        el[0].innerHTML = ''
+      }
+      
+  })
+
+}
+
+// Set a proper root
 if ( l.hash == '' ){
   l.hash = '/'
 }
 
-_.route = function route(route, query, next){
-
-  w.addEventListener("hashchange", function(){
-
-    var x = l.hash.slice(2)
-    var el = document.querySelectorAll(query)[0]
-
-    if ( route.slice(1) == a[0] ) {
-      next(el)
-    } else {
-      el.innerHTML = ''
-    }
-
-  })
-
+// Fire hashchange one to set the route
+setTimeout(function(){
   w.dispatchEvent(new HashChangeEvent("hashchange"))
-
-}
+}, 0)
