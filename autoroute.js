f6632441cd4d701e3dbfefb8af55bea38c78f357
Didier Franc <mail@didierfranc.com>
2016-02-26 17:51:14 +0100

    Update autoroute.js

--- autoroute.js
+++ autoroute.js
@@ -3,7 +3,6 @@ import Touch from 'hammerjs'
 var route = {
   create : create,
   start : start,
-  hide : hide,
   routes : {},
   t : []
 }
@@ -16,40 +15,38 @@ function create (path, fn) {
   document.getElementById('router').appendChild(e)
 
   e.innerHTML = e.html = ''
-  e.classList.add('route')
+  e.className = 'route hide'
+  e.anim = false
+  e.q = {}
 
-  e.render = function () {
+  e.render = () => {
     fn.call(e)
+    e.anim ? e.style.zIndex = '2' : e.style.zIndex = '1'
     e.innerHTML = e.html
     e.html = ''
   }
 
-  e.show = function () {
-    e.style.display = 'block'
-  }
-
-  e.q = {}
   e.render()
-
   route.routes[path] = e
+
 }
 
-function el (element, callback) {
+function el (element, fn) {
   var els = document.querySelectorAll(element)
   for (var i = 0; i < els.length; i++) {
-    callback(els[i], i)
+    fn(els[i], i)
   }
 }
 
 function linkify () {
 
-  route.t.forEach(function (e) { e.destroy() })
+  route.t.forEach( e => e.destroy() )
   route.t = []
 
-  el('[link]', function (el, i) {
+  el('[link]', (el, i) => {
 
     route.t[i] = new Touch(el, { touchAction: 'pan-x pan-y' })
-    route.t[i].on('tap', function(ev) {
+    route.t[i].on('tap', ev => {
 
       window.location.hash = '/' + ev.target.getAttribute('link')
 
@@ -57,6 +54,7 @@ function linkify () {
         document.querySelectorAll('.active')[0].classList.remove('active')
         ev.target.classList.add('active')
       }
+
     })
   })
 }
@@ -66,16 +64,10 @@ function start (path) {
   linkify()
 }
 
-function hide () {
-  for (var item in this.routes) {
-    this.routes[item].style.display = 'none'
-  }
-}
-
 export default route
 
 window.location.hash = '/'
-window.addEventListener('hashchange', function (ev) {
+window.addEventListener('hashchange', ev => {
 
   var newH = ev.newURL ? ev.newURL.split('#/')[1].split('?') : []
   var oldH = ev.oldURL ? ev.oldURL.split('#/')[1].split('?') : []
@@ -83,32 +75,40 @@ window.addEventListener('hashchange', function (ev) {
   if (route.routes[newH[0]]) {
 
     if (newH[1]) {
-
       var q = newH[1].split('&')
-
       for (var i = 0; i < q.length; i++) {
         var a = q[i].split('=')
         route.routes[newH[0]].q[a[0]] = a[1]
       }
-
-      route.hide()
       route.routes[newH[0]].render()
-      route.routes[newH[0]].show()
+    }
+
+    if (route.routes[newH[0]].anim) {
+
+      route.routes[newH[0]].classList.remove('hide')
+      route.routes[newH[0]].classList.remove('out')
+      route.routes[newH[0]].classList.add('in')
+      setTimeout(function(){ route.routes[oldH[0]].classList.add('hide') }, 300)
+
+    } else if (route.routes[oldH[0]] && route.routes[oldH[0]].anim) {
+
+      route.routes[newH[0]].classList.remove('hide')
+      route.routes[oldH[0]].classList.remove('in')
+      route.routes[oldH[0]].classList.add('out')
 
     } else {
 
-      route.hide()
-      route.routes[newH[0]].show()
+      if (route.routes[oldH[0]]) route.routes[oldH[0]].classList.add('hide')
+      route.routes[newH[0]].classList.remove('hide')
 
     }
 
   }
-
 })
 
 var h,f
-el('header', e => h = e.offsetHeight )
-el('footer', e => f = e.offsetHeight )
+el('header', e => h = e.offsetHeight)
+el('footer', e => f = e.offsetHeight)
 
 var css = document.createElement('style')
 css.innerHTML =
@@ -150,14 +150,53 @@ a {
 .route {
   display: block;
   position: absolute;
-  z-index: 1;
   width: 100%;
   height: ${ window.innerHeight - f - h - 20 + 'px' };
   overflow: scroll;
   -webkit-overflow-scrolling: touch;
   background-color: #fafafa;
   animation-duration: 0.3s;
-  animation-delay: 0s
+  animation-delay: 0s;
+  animation-fill-mode: both
+}
+
+.hide {
+  visibility : hidden
+}
+
+@-webkit-keyframes slideInRight {
+  from {
+    -webkit-transform: translate3d(100%, 0, 0);
+    transform: translate3d(100%, 0, 0);
+    visibility: visible;
+  }
+
+  to {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.in {
+  -webkit-animation-name: slideInRight;
+}
+
+@-webkit-keyframes slideOutRight {
+  from {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  to {
+    visibility: hidden;
+    -webkit-transform: translate3d(100%, 0, 0);
+    transform: translate3d(100%, 0, 0);
+  }
+}
+
+.out {
+  -webkit-animation-name: slideOutRight;
+  animation-name: slideOutRight;
 }
 `
 document.body.appendChild(css)
